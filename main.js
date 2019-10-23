bf350760ca8c392d73c2ef81def0b654095916ef
ericarthurc <hebrew.8.12@gmail.com>
2019-10-23 10:10:48 -0700

    cleaned up main.js to be stock

--- main.js
+++ main.js
@@ -2,29 +2,14 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path');
 const isDev = require('electron-is-dev');
-const chalk = require('chalk')
-
-const isWindows = (process.platform === 'win32');
-const isMacintosh = (process.platform === 'darwin');
-const isLinux = (process.platform === 'linux');
-
-console.log(chalk.green('hello!!'));
 
 let mainWindow;
 
-
 const createWindow = () => {
     mainWindow = new BrowserWindow({
-        backgroundColor: '#2A2A2A',
         width: 1280,
         height: 720,
-        minWidth: 720,
-        minHeight: 480,
-        // breaks aero snap in windows; would be used for rounded corners...
-        // transparent: isWindows,
-        // frame: isMacintosh,
         webPreferences: { nodeIntegration: true }
     });
 
